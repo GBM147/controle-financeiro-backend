62e3565e2f8fe67128fdf3dcb76bc1e22646c31a
GBM147 <parkermoraes14@gmail.com>
2026-09-21 12:38:31 -0300

    chore: atualiza cache para o dashboard interativo

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const VERSAO_TUTORIAL = '1.1.48';
-const CACHE_GBM = 'gbm-estatico-v26';
+const CACHE_GBM = 'gbm-estatico-v27';
 const PREFIXO_CACHE_GBM = 'gbm-estatico-';
 const ARQUIVOS_ESTATICOS = [
     '/index.html',
